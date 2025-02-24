3dba053ffac9abba5d8e853367920aa25e5eaf95
Divya-kaushal5150 <dk5150@nyu.edu>
2025-02-23 23:27:16 -0500

    Add files via upload

--- gettingStarted.py
+++ gettingStarted.py
@@ -5,69 +5,42 @@
 def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
-    if question == "In Slack, what is the secret passphrase posted in the lab channel? : ":
-        result1 = input(question)
-        if result1.lower() == "tiktok":
-            answer = "Correct"
-        else:
-            answer = "Incorrect"
-    
-    elif question == "Are encoding and encryption the same? - Yes/No : ":
-        result2 = input(question)
-        if result2.lower() == "no":
-            answer = "Correct"
-        else:
-            answer = "Incorrect"
-    
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No : ":
-        result3 = input(question)
-        if result3.lower() == "no":
-            answer = "Correct"
-        else:
-            answer = "Incorrect"
+    #question1
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "tiktok"
 
-    elif question == "Is it possible to decode a message without a key? - Yes/No : ":
-        result4 = input(question)
-        if result4.lower() == "yes":
-            answer = "Correct"
-        else:
-            answer = "Incorrect"
-    
-    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No : ":
-        result5 = input(question)
-        if result5.lower() == "yes":
-            answer = "Correct"
-        else:
-            answer = "Incorrect"
-    
-    elif question == "What is the SHA256 hashing value of your NYU email? - enter hashcode : ":
-        result6 = input(question)
-        if result6.lower() == "sha256":
-            answer = "Correct"
-        else:
-            answer = "Incorrect"
-    
-    elif question == "Is MD5 a secured hashing algorithm? - Yes/No : ":
-        result7 = input(question)
-        if result7.lower() == "yes":
-            answer = "Correct"
-        else:
-            answer = "Incorrect"
-    
-    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - enter integer number : ":
-        result8 = int(input(question))
-        if result8 == 4:
-            answer = "Correct"
-        else:
-            answer = "Incorrect"
-    
-    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - enter integer number : ":
-        result9 = int(input(question))
-        if result9 == 3:
-            answer = "Correct"
-        else:
-            answer = "Incorrect"
-    
+    #question2
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+
+    #question3    
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+
+    #question4  
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+
+    #question5    
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "Yes"
+
+    #question6    
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        answer = "SHA256"
+
+    #question7    
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "Yes"
+
+    #question8    
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = 4
+
+    #question9    
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = 3
+        
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
@@ -79,31 +52,31 @@ def welcome_assignment_answers(question):
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
-    question1 = "In Slack, what is the secret passphrase posted in the lab channel? : "
+    question1 = "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
     print(welcome_assignment_answers(question1))
 
-    question2 = "Are encoding and encryption the same? - Yes/No : "
+    question2 = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(question2))
 
-    question3 = "Is it possible to decrypt a message without a key? - Yes/No : "
+    question3 = "Is it possible to decrypt a message without a key? - Yes/No"
     print(welcome_assignment_answers(question3))
 
-    question4 = "Is it possible to decode a message without a key? - Yes/No : "
+    question4 = "Is it possible to decode a message without a key? - Yes/No"
     print(welcome_assignment_answers(question4))
 
-    question5 = "Is a hashed message supposed to be un-hashed? - Yes/No : "
+    question5 = "Is a hashed message supposed to be un-hashed? - Yes/No"
     print(welcome_assignment_answers(question5))
 
-    question6 = "What is the SHA256 hashing value of your NYU email? - enter hashcode : "
+    question6 = "What is the SHA256 hashing value of your NYU email and use the answer in your code - "
     print(welcome_assignment_answers(question6))
 
-    question7 = "Is MD5 a secured hashing algorithm? - Yes/No : "
+    question7 = "Is MD5 a secured hashing algorithm? - Yes/No"
     print(welcome_assignment_answers(question7))
 
-    question8 = "What layer of the TCP/IP model does the protocol DNS belong to? - enter integer number : "
+    question8 = "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number"
     print(welcome_assignment_answers(question8))
 
-    question9 = "What layer of the TCP/IP model does the protocol ICMP belong to? - enter integer number : "
+    question9 = "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number"
     print(welcome_assignment_answers(question9))
 
 #Questions:
